25ea2d114b439f7323c2c1242733ef56b803c472
Onah Chizoba Bernard <onahbernardchizoba@gmail.com>
2025-02-17 10:16:38 +0100

    fixes

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -1,48 +1,22 @@
-import React, { useContext, useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faAngleDown,
-  faAngleUp,
-  faCartShopping,
-} from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import UserContext from "../../context/UserContext";
-import Cart from "./Cart";
-import Account from "./Account";
-import { ReactComponent as Slider } from "../../assets/slider.svg";
-import "./Header.scss";
+import React, { useContext, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faAngleUp, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import UserContext from '../../context/UserContext';
+import Cart from './Cart';
+import Account from './Account';
+import { ReactComponent as Slider } from '../../assets/slider.svg';
+import './Header.scss';
 
-const Header = ({ specialsRef, aboutRef, menuCartRef, footerRef }) => {
+const Header = ({
+  specialsRef, aboutRef, menuCartRef, footerRef,
+}) => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
   const [cartIsOpen, setCartIsOpen] = useState(false);
   const [accountIsOpen, setAccountIsOpen] = useState(false);
-  const [touchStartY, setTouchStartY] = useState(0);
-  const [touchMoveY, setTouchMoveY] = useState(0);
-  const [isVerticalScroll, setIsVerticalScroll] = useState(false);
-
-  const handleTouchStart = (e) => {
-    setTouchStartY(e.touches[0].clientY);
-    setIsVerticalScroll(false); // Reset on new touch
-  };
-
-  const handleTouchMove = (e) => {
-    setTouchMoveY(e.touches[0].clientY);
-    const deltaY = Math.abs(touchMoveY - touchStartY);
-    const deltaX = Math.abs(e.touches[0].clientX - e.touches[0].clientX);
-
-    if (deltaY > deltaX) {
-      setIsVerticalScroll(true); // If vertical movement is greater, allow scrolling
-    }
-  };
-
-  const handleTouchEnd = (e) => {
-    if (isVerticalScroll) {
-      e.stopPropagation(); // Prevent carousel from capturing vertical scroll
-    }
-  };
 
   const name = localStorage.getItem("firstName");
 
@@ -55,21 +29,21 @@ const Header = ({ specialsRef, aboutRef, menuCartRef, footerRef }) => {
   let quantity = 0;
 
   cartItems.forEach((item) => {
-    quantity = item.units + quantity;
+    quantity = (item.units) + quantity;
   });
 
   useEffect(() => {
-    const itemsInCart = JSON.parse(localStorage.getItem("cartItems") || "[]");
+    const itemsInCart = JSON.parse(localStorage.getItem("cartItems") || '[]');
 
     setCartItems(itemsInCart);
     setShowPasswordField(false);
-  }, []);
+  }, [])
 
   const handleScroll = (ref) => {
     window.scrollTo({
       top: ref.current.offsetTop,
       left: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   };
 
@@ -79,135 +53,51 @@ const Header = ({ specialsRef, aboutRef, menuCartRef, footerRef }) => {
         <div className="header-content">
           <div className="top-menu  scroll-reveal">
             <div className="menu">
-              <button
-                onClick={() => setMenuIsOpen(!menuIsOpen)}
-                type="button"
-                className="title-bar"
-              >
-                <div
-                  className="menu-icon dark"
-                  type="button"
-                  data-toggle="main-nav"
-                ></div>
+              <button onClick={() => setMenuIsOpen(!menuIsOpen)} type="button" className="title-bar">
+                <div className="menu-icon dark" type="button" data-toggle="main-nav"></div>
                 <div className="title-bar-title">Menu</div>
               </button>
               <nav id="main-nav" data-animate="menu-in menu-out">
-                <ul className={`main-navigation ${menuIsOpen ? "open" : ""}`}>
-                  <li>
-                    <a
-                      className={`${cartIsOpen ? "m-anim" : "m-active"}`}
-                      onClick={() => {
-                        setCartIsOpen(false);
-                        setAccountIsOpen(false);
-                      }}
-                      data-text="Home"
-                    >
-                      Home
-                    </a>
+                <ul className={`main-navigation ${menuIsOpen ? 'open' : ''}`}>
+                  <li><a className={`${cartIsOpen ? 'm-anim' : 'm-active'}`}
+                    onClick={() => { setCartIsOpen(false); setAccountIsOpen(false); }}
+                    data-text="Home">Home</a>
                   </li>
-                  <li>
-                    <a
-                      className="m-anim"
-                      onClick={() => handleScroll(specialsRef)}
-                      data-text="Specials"
-                    >
-                      Specials
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="m-anim"
-                      onClick={() => handleScroll(menuCartRef)}
-                      data-text="Menu Cart"
-                    >
-                      Menu Cart
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="m-anim"
-                      onClick={() => handleScroll(aboutRef)}
-                      data-text="About"
-                    >
-                      About
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="m-anim"
-                      onClick={() => handleScroll(footerRef)}
-                      data-text="Contact"
-                    >
-                      Contact
-                    </a>
-                  </li>
-                  {!name ? (
-                    <li>
-                      <a
-                        className={`${accountIsOpen ? "m-active" : "m-anim"}`}
-                        onClick={() => {
-                          setAccountIsOpen(!accountIsOpen);
-                          setCartIsOpen(false);
-                        }}
-                        data-text="Account"
-                      >
-                        <FontAwesomeIcon className="icon" icon={faUser} />
-                        Account{" "}
-                        {!accountIsOpen ? (
-                          <FontAwesomeIcon className="arw" icon={faAngleDown} />
-                        ) : (
-                          <FontAwesomeIcon className="arw" icon={faAngleUp} />
-                        )}
-                      </a>
+                  <li><a className="m-anim" onClick={() => handleScroll(specialsRef)} data-text="Specials">Specials</a></li>
+                  <li><a className="m-anim" onClick={() => handleScroll(menuCartRef)} data-text="Menu Cart">Menu Cart</a></li>
+                  <li><a className="m-anim" onClick={() => handleScroll(aboutRef)} data-text="About">About</a></li>
+                  <li><a className="m-anim" onClick={() => handleScroll(footerRef)} data-text="Contact">Contact</a></li>
+                  {!name ?
+                    <li><a className={`${accountIsOpen ? 'm-active' : 'm-anim'}`}
+                      onClick={() => { setAccountIsOpen(!accountIsOpen); setCartIsOpen(false); }} data-text="Account">
+                      <FontAwesomeIcon className="icon" icon={faUser} />
+                      Account {!accountIsOpen ? <FontAwesomeIcon className="arw" icon={faAngleDown} /> :
+                        <FontAwesomeIcon className="arw" icon={faAngleUp} />}</a>
                     </li>
-                  ) : (
-                    <li>
-                      <a
-                        className={`${accountIsOpen ? "m-active" : "m-anim"}`}
-                        onClick={() => {
-                          setAccountIsOpen(!accountIsOpen);
-                          setCartIsOpen(false);
-                        }}
-                        data-text="Account"
-                      >
-                        <FontAwesomeIcon className="icon" icon={faUser} />
-                        Hi, {name}{" "}
-                        {!accountIsOpen ? (
-                          <FontAwesomeIcon className="arw" icon={faAngleDown} />
-                        ) : (
-                          <FontAwesomeIcon className="arw" icon={faAngleUp} />
-                        )}
-                      </a>
-                    </li>
-                  )}
-                  <li>
-                    <a
-                      className={`${cartIsOpen ? "m-active" : "m-anim"}`}
-                      onClick={() => {
-                        setCartIsOpen(!cartIsOpen);
-                        setAccountIsOpen(false);
-                      }}
-                      data-text="Cart"
-                    >
-                      <FontAwesomeIcon className="icon" icon={faCartShopping} />
-                      Cart{" "}
-                      {!cartIsOpen ? (
-                        <FontAwesomeIcon className="arw" icon={faAngleDown} />
-                      ) : (
-                        <FontAwesomeIcon className="arw" icon={faAngleUp} />
-                      )}
-                      {cartItems.length != 0 && (
-                        <div className="badge">
-                          <span className="bg">{quantity}</span>
-                        </div>
-                      )}
-                    </a>
+                    :
+                    <li><a className={`${accountIsOpen ? 'm-active' : 'm-anim'}`}
+                      onClick={() => { setAccountIsOpen(!accountIsOpen); setCartIsOpen(false); }} data-text="Account">
+                      <FontAwesomeIcon className="icon" icon={faUser} />
+                      Hi, {name} {!accountIsOpen ? <FontAwesomeIcon className="arw" icon={faAngleDown} /> :
+                        <FontAwesomeIcon className="arw" icon={faAngleUp} />}</a>
+                    </li>}
+                  <li><a className={`${cartIsOpen ? 'm-active' : 'm-anim'}`}
+                    onClick={() => { setCartIsOpen(!cartIsOpen); setAccountIsOpen(false); }} data-text="Cart">
+                    <FontAwesomeIcon className="icon" icon={faCartShopping} />
+                    Cart {!cartIsOpen ? <FontAwesomeIcon className="arw" icon={faAngleDown} /> :
+                      <FontAwesomeIcon className="arw" icon={faAngleUp} />}
+                    {cartItems.length != 0 &&
+                      <div className="badge">
+                        <span className="bg">{quantity}</span>
+                      </div>}</a>
                   </li>
                 </ul>
               </nav>
             </div>
-            {cartIsOpen && <Cart menuCartRef={menuCartRef} />}
-            {accountIsOpen && <Account />}
+            {cartIsOpen &&
+              <Cart menuCartRef={menuCartRef} />}
+            {accountIsOpen &&
+              <Account />}
           </div>
         </div>
       </div>
@@ -224,25 +114,12 @@ const Header = ({ specialsRef, aboutRef, menuCartRef, footerRef }) => {
         showStatus={false}
         cssEase="linear"
         showThumbs={false}
-        swipeable={!isVerticalScroll}
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
+        swipeable={false}
       >
-        <div
-          className="slider"
-          onClick={() => {
-            setCartIsOpen(false);
-            setAccountIsOpen(false);
-          }}
-        >
+        <div className="slider" onClick={() => { setCartIsOpen(false); setAccountIsOpen(false) }}>
           <div className="a-slide slide1">
             <div className="container">
-              <div
-                className="bottom-section scroll-reveal"
-                data-origin="right"
-                data-distance="20%"
-              >
+              <div className="bottom-section scroll-reveal" data-origin="right" data-distance="20%">
                 <h1 className="header-txt">
                   Fastest <span>online</span> food <span>delivery</span> service
                 </h1>
@@ -253,13 +130,7 @@ const Header = ({ specialsRef, aboutRef, menuCartRef, footerRef }) => {
             </div>
           </div>
         </div>
-        <div
-          className="slider"
-          onClick={() => {
-            setCartIsOpen(false);
-            setAccountIsOpen(false);
-          }}
-        >
+        <div className="slider" onClick={() => { setCartIsOpen(false); setAccountIsOpen(false) }}>
           <div className="a-slide slide2">
             <div className="container">
               <div className="bottom-section">
@@ -276,7 +147,7 @@ const Header = ({ specialsRef, aboutRef, menuCartRef, footerRef }) => {
       </Carousel>
     </header>
   );
-};
+}
 
 Header.propTypes = {
   specialsRef: PropTypes.object.isRequired,
